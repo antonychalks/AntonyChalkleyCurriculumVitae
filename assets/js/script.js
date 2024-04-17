1fb043f01ba477d9981008d65727ba491ab3ac85
antonychalks <antonychalkley@hotmail.co.uk>
2024-04-17 20:13:49 +0100

    Created one project card and created functionality to change the main content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
     $(".contact-body").css("height", $(".form-body").height());
   });
+
+  function displayProject(button) {
+    var projectName = button.getAttribute('data-name'); // Get the value of data-name attribute
+    console.log("Displaying project:", projectName)
+    $.get('assets/includes/' + projectName + '.html')
+    .done(function(html) {
+      console.log(html); // Log the response text
+      $("#portfolio_content").html(html); // Set the response text as the HTML content of #portfolio-content
+    })
+    .fail(function(xhr, status, error) {
+      console.error("Error loading HTML:", error);
+    });
+    
+  }
+
